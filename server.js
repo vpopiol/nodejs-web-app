92e1403f044046d3d8409cfb4292000295af93cb
Victor Popiol <vpopiol@amazon.com>
2022-01-12 14:49:39 -0500

    removed superflous routes

--- server.js
+++ server.js
@@ -9,25 +9,25 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.send(`***Hello World! You used this URL: ${req.originalUrl} Running on ${os.arch()}\n`);
 });
 
-app.get('/ecs/x86', (req, res) => {
-  res.send(`***Hello World! You used this URL: ${req.originalUrl} Running on ${os.arch()}\n`);
-});
+// app.get('/ecs/x86', (req, res) => {
+//   res.send(`***Hello World! You used this URL: ${req.originalUrl} Running on ${os.arch()}\n`);
+// });
 
-app.get('/ecs/arm', (req, res) => {
-  res.send(`***Hello World! You used this URL: ${req.originalUrl} Running on ${os.arch()}\n`);
-});
+// app.get('/ecs/arm', (req, res) => {
+//   res.send(`***Hello World! You used this URL: ${req.originalUrl} Running on ${os.arch()}\n`);
+// });
 
-app.get('/ec2/x86', (req, res) => {
-  res.send(`***Hello World! You used this URL: ${req.originalUrl} Running on ${os.arch()}\n`);
-});
+// app.get('/ec2/x86', (req, res) => {
+//   res.send(`***Hello World! You used this URL: ${req.originalUrl} Running on ${os.arch()}\n`);
+// });
 
-app.get('/ec2/arm', (req, res) => {
-  res.send(`***Hello World! You used this URL: ${req.originalUrl} Running on ${os.arch()}\n`);
-});
+// app.get('/ec2/arm', (req, res) => {
+//   res.send(`***Hello World! You used this URL: ${req.originalUrl} Running on ${os.arch()}\n`);
+// });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
